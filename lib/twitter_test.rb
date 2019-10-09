e006b6f1e311f536ec9e441e748ed1ad4c3b17b9
hermine-dm <herdementh@gmail.com>
2019-10-09 15:48:41 +0200

    ok twiiter tout sur un meme fichier

---
+++ lib/twitter_test.rb
@@ -0,0 +1,101 @@
+# ligne très importante qui appelle la gem.
+require 'twitter'
+require 'dotenv'
+require 'pry'
+# n'oublie pas les lignes pour Dotenv ici…
+Dotenv.load('.env') 
+
+ 
+# quelques lignes qui appellent les clés d'API de ton fichier .env
+#def de la fonction login_twitter
+def login_twitter
+	client = Twitter::REST::Client.new do |config|
+	  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+	  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+	  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+	  config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+	end
+	return client
+end
+
+# ligne qui permet de tweeter sur ton compte
+#client.update('Mon premier tweet en Ruby !!!!')
+
+#def pour envoyer à nb journalistes d'un array, un string défini
+def find_x_j(array,nb)
+	array_of_x_journalist = array.sample(nb)
+	return array_of_x_journalist
+end
+
+
+def twitt_string(string, array, client)
+	for i in 0...array.size
+		client.update("#{string} #{array[i]}")
+	end
+end
+
+def perform(string,array,nb)
+	client = login_twitter()
+	array_of_x_journalist = find_x_j(array,nb) 
+	twitt_string(string,array_of_x_journalist, client)
+end
+
+#array_of_journalist = ["@jcunniet", "@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain"]
+#perform("merci pour l'article #bonjour_monde",array_of_journalist,5)
+#ok ca a bien marché
+
+
+#def pour follow nb personnes qui ont tweeté un string
+def finds_the_tweets(string,nb)
+	client = login_twitter()
+	client.search(string, result_type: "recent").take(nb).each do |tweet|
+  	 	client.fav(tweet)
+	end
+end
+
+#finds_the_tweets("#bonjour_monde",25)
+# ok ça a marché
+
+def follow_the_tweets(string,nb)
+	client = login_twitter()
+	a = 1
+	array = []
+	until array.size > nb
+		client.search(string, result_type: "recent").take(a).each do |tweet|
+  	 		array.push("#{tweet.user.screen_name}")
+  	 		array = array.uniq
+  	 		a +=1
+  	 	end
+	end
+	for i in 0...nb
+		puts array[i]
+		client.follow("#{array[i]}")
+	end
+
+end
+#follow_the_tweets("#bonjour_monde",10)
+
+def login_stream()
+	client = Twitter::Streaming::Client.new do |config|
+  		config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+  		config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+  		config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+  		config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  	end
+  	return client
+end
+
+
+def stream_twitt(string)
+	client_stream = login_stream()
+	client = login_twitter()
+	client_stream.filter(track: string) do |object|
+  		if object.is_a?(Twitter::Tweet)
+  			puts object.text 
+  			client.fav(object)
+  			client.follow(object.user)
+  		end
+  	end
+end
+#stream_twitt("#bonjour_monde")
+#ok ca marche 
